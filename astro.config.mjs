687f8eebf1c711e1d0afa75c1646627e79abcdb5
Admink <admink@asdata.xyz>
2023-09-04 18:48:27 +0100

    add icon.astro y menus segun idioma de la web

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.iampedal.com",
