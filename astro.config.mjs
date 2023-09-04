fc5f6d2c3a1e11035290242b9680d9b2ecf0a0f5
Admink <admink@asdata.xyz>
2023-09-04 16:42:19 +0100

    add astro navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.iampedal.com",
   integrations: [
     tailwind(), 
     sitemap()
